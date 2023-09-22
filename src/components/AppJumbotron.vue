6307aa7c33561842528d4f08cd0276cd826cab81
Riccardo <riky.volto@live.com>
2023-09-22 17:25:24 +0200

    add jumbotron component

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -18,7 +18,41 @@ export default{
 </script>
 
 <template>
+    <section class="py-5">
+        <div class="container w-75 d-flex align-items-center justify-content-between">
+            <div class="d-flex align-items-center">
+                <img src="../img/buy-comics-digital-comics.png" alt="">
+                <div class="ps-3">DIGITAL COMICS</div>
+            </div>
+            <div class="d-flex align-items-center">
+                <img src="../img/buy-comics-merchandise.png" alt="">
+                <div class="ps-3">DC MERCHANDISE</div>
+            </div>
+            <div class="d-flex align-items-center">
+                <img src="../img/buy-comics-subscriptions.png" alt="">
+                <div class="ps-3">SUBSCRIPTION</div>
+            </div>
+            <div class="d-flex align-items-center">
+                <img src="../img/buy-comics-shop-locator.png" alt="">
+                <div class="ps-3">COMIC SHOP LOCATOR</div>
+
+
+            </div>
+            <div class="d-flex align-items-center">
+                <img src="../img/buy-dc-power-visa.svg" alt="">
+                <div class="ps-3">DC POWER VISA</div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
+    img {
+        width: 50px;
+    }
+    section {
+        font-size: 10px;
+        color: white;
+        background-color: #0282f9;
+    }
 </style>
--- src/components/AppMainContent.vue
+++ src/components/AppMainContent.vue
@@ -18,7 +18,19 @@ export default{
 </script>
 
 <template>
+    <main class="bg-black">
+        <div class="container w-75 h-100 d-flex align-items-center">
+            <h2>Content goes here</h2>
+        </div>
+    </main>
 </template>
 
 <style lang="scss" scoped>
+    main {
+        height: 100px;
+        
+        h2 {
+            color: white;
+        }
+    }
 </style>
