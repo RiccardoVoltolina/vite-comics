7d5a4f2bf1f9510758889d30d3077f8a2c1247c2
Riccardo <riky.volto@live.com>
2023-09-25 13:27:41 +0200

    add title on images

--- src/components/AppMainContent.vue
+++ src/components/AppMainContent.vue
@@ -21,6 +21,19 @@ export default{
 <template>
     <main class="">
         <section class="bg_image"></section>
+        <section class="bg-black">
+            <div class="container w-75 g-0">
+                <div class="row g-0">
+                    <div class="col-2 p-3" v-for="(card, i) in cards.cards">
+                        <div class="d-flex flex-column">
+                            <img :src="card.thumb" alt="">
+                            <h5 class="text-white">{{ card.series }}</h5>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </section>
 
     </main>
 </template>
@@ -31,6 +44,16 @@ export default{
             height: 300px;
             background-image: url(../img/jumbotron.jpg);
             background-size: cover;
+        }
+
+        img {
+            max-width: 300px;
+            aspect-ratio: 1;
+        }
+
+        h5 {
+            font-size: 13px;
+            padding-top: 1rem;
         }
     }
 </style>
